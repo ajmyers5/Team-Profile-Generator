e8e44ddbf0acefa3252415dd9975bd4e4347a0a4
Andrew Myers <ajmyers5@gmail.com>
2020-05-09 16:42:35 -0500

    app.ja file

--- Develop/lib/mngm.js
+++ Develop/lib/mngm.js
@@ -1,16 +1,17 @@
+  
 const Employee = require("./Employee");
 
-class Manager extends Employee {
-  constructor(id, name, email, officenum) {
-    super(id, name, email, "Manager");
-    this.officeNum = officeNum;
-  }
-  getRole() {
-    return "Manager";
-  }
-  getOfficeNumber() {
-    return this.officeNum;
-  }
+class Manager extends Employee  {
+    constructor(id, name, email, officenum) {
+        super(id, name, email, 'Manager');
+        this.officeNum = officeNum;
+    }
+    getRole() {
+        return "Manager";
+    }
+    getOfficeNumber() {
+        return this.officeNum;
+    }
 }
-
+ 
 module.exports = Manager;
